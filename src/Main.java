6add35ae28151738a751a74af2b5edf9c88b0a09
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-08-30 20:13:52 +0300

    inta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("San berin");
+
+        while (true) {
+            int a = scanner.nextInt();
+            System.out.println("San berin");
+            int b = scanner.nextInt();
+            int v = a + b;
+            if (v <= 0)
+                try {
+                    throw new ArithmeticException("folse " + v);
+                } catch (ArithmeticException e) {
+                    System.out.println("Povtor " + v);
+                }
+            }
+        }
     }
-}
